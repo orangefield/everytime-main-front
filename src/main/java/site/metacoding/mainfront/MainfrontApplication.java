dcd34f86727106fe261a23fbed502dc3b05ef661
field <jsw_777@naver.com>
2022-04-04 11:33:19 +0900

    1. 프로젝트 세팅

---
+++ src/main/java/site/metacoding/mainfront/MainfrontApplication.java
@@ -0,0 +1,13 @@
+package site.metacoding.mainfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainfrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainfrontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/metacoding/mainfront/MainfrontApplicationTests.java
@@ -0,0 +1,13 @@
+package site.metacoding.mainfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainfrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
